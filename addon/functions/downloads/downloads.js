dc9af8c474c11c97d59ad779e8acd252e819ad6f
Abdullah Muhamed <cdumb@proton.me>
2026-05-27 02:29:18 +0300

    rely on chrome.download.onCreated and skip blobs

--- addon/functions/downloads/downloads.js
+++ addon/functions/downloads/downloads.js
@@ -11,74 +11,31 @@ const Downloads = (() => {
     }
   }
 
-  function getHeader(headers, name) {
-    return (
-      headers?.find((h) => h.name.toLowerCase() === name.toLowerCase())
-        ?.value ?? null
+  async function onCreated(downloadItem) {
+    if (!_enabled) return;
+    if (downloadItem.url.startsWith("blob:")) return;
+    if (downloadItem.fileSize > 0 && downloadItem.fileSize < 524288) return;
+
+    const host = getHost(downloadItem.url);
+    if (_blacklist.includes(host)) return;
+
+    await browser.downloads.cancel(downloadItem.id);
+    await browser.downloads.erase({ id: downloadItem.id });
+
+    recordHistory(host);
+
+    _send(
+      "downloads.add",
+      JSON.stringify({
+        id: String(downloadItem.id),
+        url: downloadItem.url,
+        filename: downloadItem.filename.split(/[\\/]/).pop(),
+        mime: downloadItem.mime,
+        fileSize: downloadItem.fileSize,
+        referrer: downloadItem.referrer,
+        headers: { Referer: downloadItem.referrer },
+      }),
     );
-  }
-
-  function isDownload(details) {
-    const disposition =
-      getHeader(details.responseHeaders, "content-disposition") ?? "";
-    if (/attachment/i.test(disposition)) return true;
-
-    const mime = (getHeader(details.responseHeaders, "content-type") ?? "")
-      .split(";")[0]
-      .trim()
-      .toLowerCase();
-
-    const DOWNLOAD_MIMES = [
-      "application/octet-stream",
-      "application/x-msdownload",
-      "application/x-binary",
-      "application/macbinary",
-      "application/x-tar",
-      "application/x-gzip",
-      "application/x-bzip2",
-      "application/x-xz",
-      "application/x-7z-compressed",
-      "application/zip",
-      "application/x-rar-compressed",
-      "application/vnd.rar",
-      "application/x-iso9660-image",
-      "application/x-apple-diskimage",
-      "application/vnd.android.package-archive",
-      "application/x-rpm",
-      "application/x-debian-package",
-      "application/x-executable",
-      "application/x-sharedlib",
-      "application/x-msdos-program",
-      "application/x-dosexec",
-    ];
-
-    return DOWNLOAD_MIMES.includes(mime);
-  }
-
-  function parseFilename(details) {
-    const disposition =
-      getHeader(details.responseHeaders, "content-disposition") ?? "";
-
-    const utf8Match = disposition.match(/filename\*=UTF-8''([^;\r\n]+)/i);
-    if (utf8Match) {
-      try {
-        return decodeURIComponent(utf8Match[1].trim());
-      } catch {}
-    }
-
-    const quotedMatch = disposition.match(/filename="([^"]+)"/i);
-    if (quotedMatch) return quotedMatch[1].trim();
-
-    const plainMatch = disposition.match(/filename=([^;\r\n]+)/i);
-    if (plainMatch) return plainMatch[1].trim();
-
-    try {
-      const u = new URL(details.url);
-      const last = u.pathname.split("/").filter(Boolean).pop();
-      if (last) return decodeURIComponent(last);
-    } catch {}
-
-    return "";
   }
 
   async function recordHistory(host) {
@@ -88,83 +45,19 @@ const Downloads = (() => {
     browser.storage.local.set({ "downloads.history": history });
   }
 
-  function onHeadersReceived(details) {
-    if (!_enabled) return {};
-
-    const IGNORED_TYPES = [
-      "stylesheet",
-      "image",
-      "font",
-      "media",
-      "websocket",
-      "csp_report",
-      "ping",
-    ];
-    if (IGNORED_TYPES.includes(details.type)) return {};
-
-    if (!isDownload(details)) return {};
-
-    const host = getHost(details.url);
-    const referer =
-      getHeader(details.responseHeaders, "referer") ??
-      getHeader(details.requestHeaders, "referer") ??
-      "";
-    const origin = getHeader(details.requestHeaders, "origin") ?? "";
-    const effectiveHost = origin ? getHost(origin) : host;
-
-    if (_blacklist.includes(host) || _blacklist.includes(effectiveHost))
-      return {};
-
-    recordHistory(effectiveHost);
-
-    const mime = (
-      getHeader(details.responseHeaders, "content-type") ??
-      "application/octet-stream"
-    )
-      .split(";")[0]
-      .trim();
-    const contentLength = getHeader(details.responseHeaders, "content-length");
-    const filename = parseFilename(details);
-
-    _send(
-      "downloads.add",
-      JSON.stringify({
-        id: details.requestId,
-        url: details.url,
-        filename,
-        mime,
-        fileSize: contentLength ? parseInt(contentLength, 10) : -1,
-        referrer: referer,
-        headers: {
-          Referer: referer,
-          Origin: origin,
-        },
-      }),
-    );
-
-    return { cancel: true };
-  }
-
   function applyConfig(cfg) {
     _enabled = cfg["downloads.enabled"] ?? _enabled;
-    _blacklist = cfg["downloads.blacklist"] ?? _blacklist;
+    _blacklist = cfg["downloads.blacklist"] ?? [];
   }
 
   function init(send) {
     _send = send;
-
     browser.storage.local
       .get([...Object.keys(CONFIG_DEFAULTS), "downloads.blacklist"])
       .then((stored) =>
         applyConfig(Object.assign({}, CONFIG_DEFAULTS, stored)),
       );
-
-    browser.webRequest.onHeadersReceived.addListener(
-      onHeadersReceived,
-      { urls: ["<all_urls>"] },
-      ["blocking", "responseHeaders"],
-    );
-
+    browser.downloads.onCreated.addListener(onCreated);
     browser.runtime.onMessage.addListener((msg) => {
       if (msg.type === "settings_updated") applyConfig(msg.settings);
       if (msg.type === "blacklist_updated") _blacklist = msg.blacklist ?? [];
@@ -172,7 +65,7 @@ const Downloads = (() => {
   }
 
   function destroy() {
-    browser.webRequest.onHeadersReceived.removeListener(onHeadersReceived);
+    browser.downloads.onCreated.removeListener(onCreated);
     _send = null;
   }
 
